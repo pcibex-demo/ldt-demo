75ca3479361adc3757f086449f8ba937c161c6ac
pcibex-demo <67057233+pcibex-demo@users.noreply.github.com>
2020-07-01 13:19:21 +0900

    Add files via upload

--- data_includes/ldt_pcibex.js
+++ data_includes/ldt_pcibex.js
@@ -1,4 +1,4 @@
-PennController.Debug()
+DebugOff()
 
 PennController.ResetPrefix(null); // Initiates PennController
 
@@ -10,11 +10,11 @@ newTrial( "intro" ,
     defaultText
         .print()
     ,
-    newText("<p>In this experiment, you will have to report which of two pictures matches a description.</p>")
+    newText("<p>この実験では，画像の後に呈示される単語が実際にある単語かどうかをできるだけ早く判断していただきます。</p>")
     ,
-    newText("<p>Press the <strong>F</strong> key for the picture on the left, or the <strong>J</strong> key for the picture on the right.</p>")
+    newText("<p>実際にある単語だと思ったら"F"のキーを，実際にはない単語だと思ったら"J"のキーを押してください。</p>")
     ,
-    newText("<p>Please enter your ID and then click the button below to start the experiment.</p>")
+    newText("<p>IDを入力してから，下にあるstartボタンを押してください，</p>")
     ,
     newTextInput("inputID")
         .print()
@@ -33,14 +33,14 @@ newTrial( "intro" ,
 CheckPreloaded("target", "filler")
     .label( "preload" );
 
-newTrial( "welcome" ,
-    newText( "message" , "Welcome. The resources are currently being preloaded. The next trial won't start before all the resources for the 'practice' trial are loaded (i.e. 1fishSquareTank.png).")
-        .print()
-    ,
-    newButton("start", "Start")
-        .print()
-        .wait()
-);
+// newTrial( "welcome" ,
+//     newText( "message" , "Welcome. The resources are currently being preloaded. The next trial won't start before all the resources for the 'practice' trial are loaded (i.e. 1fishSquareTank.png).")
+//         .print()
+//     ,
+//     newButton("start", "Start")
+//         .print()
+//         .wait()
+// );
 
 // ターゲット用のテンプレート
 Template("target.csv", variable =>
